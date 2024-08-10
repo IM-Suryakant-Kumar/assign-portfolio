37a5ace7287bfa5e65c6713a63184cce18493da0
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2024-08-10 21:07:12 +0530

    setup gh-pages

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,24 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+// eslint.config.js
+import react from 'eslint-plugin-react'
 
 export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
-  files: ['**/*.{ts,tsx}'],
-  ignores: ['dist'],
   languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
+    // other options...
+    parserOptions: {
+      project: ['./tsconfig.node.json', './tsconfig.app.json'],
+      tsconfigRootDir: import.meta.dirname,
+    },
   },
+  // Set the react version
+  settings: { react: { version: '18.3' } },
   plugins: {
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
+    // Add the react plugin
+    react,
   },
   rules: {
-    ...reactHooks.configs.recommended.rules,
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    // other rules...
+    // Enable its recommended rules
+    ...react.configs.recommended.rules,
+    ...react.configs['jsx-runtime'].rules,
   },
 })
